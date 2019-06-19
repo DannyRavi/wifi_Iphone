6232a815eec44ddc6067f225fb9133d8c0cb8cf7
Ahmadi-PC <r.ahmadi89@gmail.com>
2019-06-19 10:53:45 +0430

    Add soft Serial

--- TCPT/TCPT.ino
+++ TCPT/TCPT.ino
@@ -7,17 +7,23 @@
 */
  
 #include <ESP8266WiFi.h>
-#include "RTPPacket.h"
+#include <SoftwareSerial.h>
+//#include "RTPPacket.h"
 #define SendKey 0  //Button to send data Flash BTN on NodeMCU
+#define SOFT_SERIAL_SPEED 115200  //should be match to target device
  
 int port = 8888;  //Port number
 WiFiServer server(port);
+
+SoftwareSerial ss(D6,D5); // D6=rx , D5=tx
+
 uint8_t buff[50];
 //Server connect to WiFi Network
 const char *ssid = "MobinNet0ECA";  //Enter your wifi SSID
 const char *password = "EFFE0ECA";  //Enter your wifi Password
  
 int count=0;
+int data_soft_serial;
 //=======================================================================
 //                    Power on setup
 //=======================================================================
@@ -49,6 +55,12 @@ void setup()
   Serial.print(WiFi.localIP());
   Serial.print(" on port ");
   Serial.println(port);
+
+  //soft Serial Setup _ should be the same as target device
+  ss.begin(SOFT_SERIAL_SPEED);
+  Serial.begin(SOFT_SERIAL_SPEED);
+  //////////////////
+
 }
 //=======================================================================
 //                    Loop
@@ -57,23 +69,35 @@ void setup()
 void loop() 
 {
   WiFiClient client = server.available();
-  
+  char vol; // 8-bits data recieved from server (Volume)
+
+
+
+
+
+  /*
   if (client) {
     if(client.connected())
     {
       Serial.println("Client Connected");
     }
-    int sensorValue = analogRead(A0);
+    //int sensorValue = analogRead(A0);
     // Serial.print(sensorValue);
     while(client.connected()){      
       while(client.available()>0){
         // read data from the connected client
-        Serial.write(client.read()); 
-      }
+        //Serial.write(client.read());
+         vol=client.read(); //recieve data from server
+    */     if (ss.available()>0)
+          {
+            data_soft_serial=ss.write(vol); // send to DAC
+            Serial.println(vol); // debug
+          }
+ /*     }
       //Send Data to connected client
       //// while(Serial.available()>0)
       //// {
-        int sensorValue = analogRead(A0);
+      //  int sensorValue = analogRead(A0);
         int numberChar = 5;
        //// client.write((int)sensorValue);
       //! RTPPacket((uint8_t *)"Hello", 0, 0).serialize(buff);
@@ -83,5 +107,5 @@ void loop()
     }
     client.stop();
     Serial.println("Client disconnected");    
-  }
+  }*/
 }
