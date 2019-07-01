15b1b04a2f8bb15e29808ea309b4f0c48f59e75c
Ahmadi-PC <r.ahmadi89@gmail.com>
2019-07-01 11:46:43 +0430

    Recieve ascii code from server and convert to numbers Also send mic sound to server

--- TCPT/TCPT.ino
+++ TCPT/TCPT.ino
@@ -6,12 +6,8 @@
 */
  
 #include <ESP8266WiFi.h>
-#include <SoftwareSerial.h>
 //#include "RTPPacket.h"
 #define SendKey 0  //Button to send data Flash BTN on NodeMCU
-#define SOFT_SERIAL_SPEED 115200  //should be match to target device
-
-SoftwareSerial ss(D6,D5); // D6=rx , D5=tx
 
 int port = 8888;  //Port number
 WiFiServer server(port);
@@ -21,7 +17,7 @@ const char *ssid = "MobinNet0ECA";  //Enter your wifi SSID
 const char *password = "EFFE0ECA";  //Enter your wifi Password
  
 int count=0;
-int data_soft_serial;
+int data_serial=0;
 //=======================================================================
 //                    Power on setup
 //=======================================================================
@@ -40,7 +36,7 @@ void setup()
     delay(500);
     Serial.print(".");
     delay(500);
-  }
+ }
  
   Serial.println("");
   Serial.print("Connected to ");
@@ -55,9 +51,6 @@ void setup()
   Serial.println(port);
 
 
-  ss.begin(SOFT_SERIAL_SPEED);    //soft Serial Setup _ should be the same as target device
-  
-
 
 }
 //=======================================================================
@@ -67,31 +60,53 @@ void setup()
 void loop() 
 {
   WiFiClient client = server.available();
-  
+ 
+  char digit[2]; // 2 digit recieve 
+  char vol;
+  int micValue=0;
+
   if (client) {
     if(client.connected())
     {
       Serial.println("Client Connected");
     }
-    int sensorValue = analogRead(A0);
+    //int sensorValue = analogRead(A0);
     // Serial.print(sensorValue);
     while(client.connected()){      
-      while(client.available()>0){
+      while(client.available()>1){
         // read data from the connected client
-        Serial.write(client.read()); 
-      }
+        //Serial.write(client.read());
+
+        //////////////////////////
+        // Serial.write(vol-48);  //  ASCII TO BINARY
+        ///////////////////////////
+
+        
+         digit[1]=client.read()-48; //recieve data from server and convert ASCII to number 
+         digit[0]=client.read()-48; // Yekaan //  convert ASCII to number 
+         vol=digit[1]*10+digit[0];  //convert array to int
+         //Serial.println(vol);
+         Serial.write(vol);
+
+
+       }
       //Send Data to connected client
+
+         micValue=analogRead(A0);
+         client.write(micValue);
+         client.write(13);
       //// while(Serial.available()>0)
       //// {
-        int sensorValue = analogRead(A0);
-        int numberChar = 5;
+      //  int sensorValue = analogRead(A0);
+     //**   int numberChar = 5;
        //// client.write((int)sensorValue);
       //! RTPPacket((uint8_t *)"Hello", 0, 0).serialize(buff);
       //! client.write(buff,50);
-      client.write(numberChar);
+      //**client.write(numberChar);
       //// }
     }
     client.stop();
     Serial.println("Client disconnected");    
   }
 }
+
